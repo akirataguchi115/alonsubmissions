902cc3b9585e114940cb67ecb78c62c78acce364
Akira Taguchi <akirahattivatti@gmail.com>
2021-01-14 15:17:01 +0200

    fixed tabs

--- 01weirdalgorithm.cpp
+++ 01weirdalgorithm.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 int main() {
-				long n;
-				cin >> n;
-				while (n != 1) {
-								cout << n << " ";	
-								if (n%2 == 0) {
-												n /= 2;
-								} else {
-												n *= 3;
-												n++;
-								}
-				}
-				cout << " 1\n";
+    long n;
+    cin >> n;
+    while (n != 1) {
+        cout << n << " ";	
+        if (n%2 == 0) {
+            n /= 2;
+        } else {
+            n *= 3;
+            n++;
+        }
+    }
+    cout << " 1\n";
 }
